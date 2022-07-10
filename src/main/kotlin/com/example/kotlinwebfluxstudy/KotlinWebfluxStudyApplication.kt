e6aeb15cacaa56181a7b9c7cbe641d7f2b1b7fd8
hyoj <chohyojoy@gmail.com>
2022-07-10 23:13:47 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/kotlinwebfluxstudy/KotlinWebfluxStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinwebfluxstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinWebfluxStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinWebfluxStudyApplication>(*args)
+}
